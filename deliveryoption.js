35403feb9f2a0120fa4a6a3b84f7a7ab50bf7468
Dave Potts <dave@goopot.co.uk>
2018-03-03 11:11:50 +0000

    Automatically picking the delivery option.

---
+++ deliveryoption.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    "use strict";
+    
+    pick_fast_ticket_delivery();
+    function pick_fast_ticket_delivery() {
+        const option = document.getElementById("KIOSKDeliveryMethod")
+        if (option) {
+            option.click();
+        }
+    }
+};
